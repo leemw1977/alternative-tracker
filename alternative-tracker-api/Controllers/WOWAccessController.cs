ae4bf60af0053c64652dae991b8b15364417756d
Lee Williams <leemw1977@outlook.com>
2020-08-20 16:05:48 +0100

    add initial

---
+++ alternative-tracker-api/Controllers/WOWAccessController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace alternative_tracker_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WOWAccessController : ControllerBase
+    {
+        private readonly ILogger<WOWAccessController> _logger;
+
+        public WOWAccessController(ILogger<WOWAccessController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get() => "it lives!";
+    }
+}
---
+++ alternative-tracker-domain/WOWAccessToken.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace alternative_tracker_domain
+{
+    public class WOWAccesstoken
+    {
+        public DateTime Expiry { get; }
+        public string Token { get; }
+
+        public WOWAccessToken(string token, DateTime expiry)
+        {
+            if (string.IsNullOrWhiteSpace(token)) throw new ArgumentException($"'{nameof(token)}' cannot be null or whitespace", nameof(token));
+            
+            Token = token;
+            Expiry = expiry;
+        }
+    }
+}
